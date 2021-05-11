83ceb4c6fd434eed1e58ec19084966b9ab2a1e4a
DOLLAr74 <greh.iv.2007@gmail.com>
2021-05-11 20:22:06 +0300

    first commit

---
+++ Bot.py
@@ -0,0 +1,23 @@
+import pyautogui, time
+input()
+time.sleep(1)
+while True:
+
+    pyautogui.press("@")
+    pyautogui.press("S")
+    pyautogui.press("t")
+    pyautogui.press("e")
+    pyautogui.press("enter")
+
+    pyautogui.press("l")
+    pyautogui.press("o")
+    pyautogui.press("h")
+    pyautogui.press("space")
+    pyautogui.press("l")
+    pyautogui.press("o")
+    pyautogui.press("s")
+    pyautogui.press("h")
+    pyautogui.press("a")
+    pyautogui.press("r")
+    pyautogui.press("a")
+    pyautogui.press("enter")
